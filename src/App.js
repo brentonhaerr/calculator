e7f7da12e5eb9f606af28cb3a4ed27c46eb2cefa
Brenton Haerr <brenton.haerr@gmail.com>
2022-05-04 10:31:45 -0700

    Added font, file structure, and display.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import Display from './components/Display';
 
 
 function App() {
   return (
-    <div className="App container-fluid">
-      <div className="row justify-content-md-center">
-        <div class="col col-md-6">
-          <h1 className="text-muted">Hello world.</h1>
-          <p className="lead">My name is Brenton.</p>
-          <p>I am a front-end developer with an emphasis on Javascript, React, and node. I bring a decade of professional office experience and a passion for web development.</p>
-        </div>
-      </div>
+    <div id="calculator" className="App container p-3">
+     <Display />
     </div>
   );
 }
---
+++ src/components/Display.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Display = () => {
+  return (
+    <div id="display">
+      <div className="row justify-content-center">
+        <div className="col-11 text-end fs-1">
+          0
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Display;
