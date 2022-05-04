78cecf771da68d4c1953caaba44a1882c01cd053
Brenton Haerr <brenton.haerr@gmail.com>
2022-05-04 10:10:29 -0700

    Cleared unneeded files.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid">
+      <div className="row justify-content-md-center">
+        <div class="col col-md-6">
+          <h1 className="text-muted">Hello world.</h1>
+          <p className="lead">My name is Brenton.</p>
+          <p>I am a front-end developer with an emphasis on Javascript, React, and node. I bring a decade of professional office experience and a passion for web development.</p>
+        </div>
+      </div>
     </div>
   );
 }
