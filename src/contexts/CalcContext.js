4a3971f6b31b156e500cab04cf2f8fb4331b6776
Brenton Haerr <brenton.haerr@gmail.com>
2022-05-04 11:07:39 -0700

    Began implementing input feature.

--- src/contexts/CalcContext.js
+++ src/contexts/CalcContext.js
@@ -4,10 +4,10 @@ import { calcReducer } from '../reducers/calcReducer';
 export const CalcContext = createContext();
 
 const CalcContextProvider = (props) => {
-  const [total, dispatch] = useReducer(calcReducer, 0);
+  const [{total, input}, dispatch] = useReducer(calcReducer, {total: 0, input: 0});
 
   return ( 
-    <CalcContext.Provider value={{ total, dispatch }}>
+    <CalcContext.Provider value={{ total, input, dispatch }}>
       { props.children }
     </CalcContext.Provider>
    );
--- src/reducers/calcReducer.js
+++ src/reducers/calcReducer.js
@@ -1,11 +1,11 @@
 export const calcReducer = (state, action) => {
   switch (action.type) {
     case "ADD":
-      return state + parseInt(action.value);
+      return {total: state.total + parseInt(action.value), input: 0}
     case "SUBTRACT":
-      return state - parseInt(action.value);
+      return {total: state.total - parseInt(action.value), input: 0}
     case "CLEAR":
-      return 0;
+      return {total: state.total, input: 0};
     default:
       return state;
   }
