3d82354a813b646917ae304bd70e729ffdabd63c
Brenton Haerr <brenton.haerr@gmail.com>
2022-05-07 11:02:26 -0700

    Improved addition/subtraction/equals functionality.

--- src/reducers/calcReducer.js
+++ src/reducers/calcReducer.js
@@ -2,8 +2,8 @@ import * as modes from '../constants/calc_modes';
 import { evaluate, mode } from 'mathjs';
 
 
-function strip_zeroes (state, action) {
-  if ( state.display == 0) {
+function strip_zeroes(state, action) {
+  if (state.display == 0) {
     return action.value;
   } else {
     return state.display + action.value;
@@ -20,19 +20,30 @@ export const calcReducer = (state, action) => {
     case modes.NUMBER_ENTRY: {
       if (state.mode == modes.DISPLAY_INPUT) {
         // Just add to the current input if we're already inputting.
-        return {...state, display: strip_zeroes(state, action)}
+        return { ...state, display: strip_zeroes(state, action) }
       } else if (state.mode == modes.DISPLAY_TOTAL) {
         // Remove the displayed total if on total mode and switch to input.
-        return {...state, display: strip_zeroes(state, action), mode: modes.DISPLAY_INPUT}
+        return { ...state, display: action.value, mode: modes.DISPLAY_INPUT }
       }
     }
     case modes.EQUALS:
       let total = evaluate(state.ops_list + state.display);
-      return {...state, display: 0, ops_list: state.ops_list + state.display, total: total, mode: modes.DISPLAY_TOTAL }
+      return { ...state, display: 0, ops_list: "", total: total, mode: modes.DISPLAY_TOTAL }
     case modes.ADD:
-      return { ...state, display: 0, ops_list: state.ops_list + state.display + "+", mode: modes.DISPLAY_TOTAL }
+      if (state.mode === modes.DISPLAY_INPUT) {
+        return { ...state, display: 0, ops_list: state.ops_list + state.display + "+", mode: modes.DISPLAY_TOTAL }
+      } else {
+        // CLear the ops list if we're continuing right after displaying a total.
+        return { ...state, display: 0, ops_list: state.total + "+", mode: modes.DISPLAY_TOTAL }
+      }
     case modes.SUBTRACT:
-      return { ...state, display: 0, ops_list: state.ops_list + state.display + "-", mode: modes.DISPLAY_TOTAL }
+      if (state.mode === modes.DISPLAY_INPUT) {
+        return { ...state, display: 0, ops_list: state.ops_list + state.display + "-", mode: modes.DISPLAY_TOTAL }
+      } else {
+        // CLear the ops list if we're continuing right after displaying a total.
+        return { ...state, display: 0, ops_list: state.total + "-", mode: modes.DISPLAY_TOTAL }
+      }
+
     case modes.CLEAR:
       return { ...state, display: 0, mode: modes.DISPLAY_INPUT };
     case modes.ALLCLEAR:
