6f45dae3b7c10e277eee3abbdf357e16ad0d8f8f
Brenton Haerr <brenton.haerr@gmail.com>
2022-05-15 13:15:27 -0700

    Removed BS styling for buttons. Created ControlPad component.

---
+++ src/components/ControlPad.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import NumpadButton from './NumpadButton';
+import FunctionButton from './FunctionButton';
+import * as acts from '../constants/calc_actions';
+
+const ControlPad = () => {
+  return (
+    <section id="control-pad">
+      <div className="row">
+        <div className="col-6">
+          <FunctionButton id="display-clear" action={acts.CLEAR} name="C" />
+        </div>
+        <div className="col-6">
+          <FunctionButton id="clear" action={acts.ALLCLEAR} name="AC" />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-3">
+          <NumpadButton id="seven" value="7" />
+        </div>
+        <div className="col-3">
+          <NumpadButton id="eight" value="8" />
+        </div>
+        <div className="col-3">
+          <NumpadButton id="nine" value="9" />
+        </div>
+        <div className="col-3">
+          <FunctionButton id="add" action={acts.ADD} name="+" />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-3">
+          <NumpadButton id="four" value="4" />
+        </div>
+        <div className="col-3">
+          <NumpadButton id="five" value="5" />
+        </div>
+        <div className="col-3">
+          <NumpadButton id="six" value="6" />
+        </div>
+        <div className="col-3">
+          <FunctionButton id="subtract" action={acts.SUBTRACT} name="-" />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-3">
+          <NumpadButton id="one" value="1" />
+        </div>
+        <div className="col-3">
+          <NumpadButton id="two" value="2" />
+        </div>
+        <div className="col-3">
+          <NumpadButton id="three" value="3" />
+        </div>
+        <div className="col-3">
+          <FunctionButton id="multiply" action={acts.MULTIPLY} name="&#215;" />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-3">
+          <NumpadButton id="zero" value="0" />
+        </div>
+        <div className="col-3">
+          <FunctionButton id="decimal" action={acts.DECIMAL} name="." />
+        </div>
+        <div className="col-3">
+          <FunctionButton id="equals" action={acts.EQUALS} name="=" />
+        </div>
+        <div className="col-3">
+          <FunctionButton id="divide" action={acts.DIVIDE} name="/" />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ControlPad;
--- src/components/NumpadButton.js
+++ src/components/NumpadButton.js
@@ -6,7 +6,7 @@ const NumpadButton = (props) => {
   const { dispatch } = useContext(CalcContext);
 
   return (
-      <button id={props.id} className="m-1 btn btn-primary btn-lg"
+      <button id={props.id} className=""
         onClick={() => {
           dispatch({ type: acts.NUMBER_ENTRY, value: props.value })
         }}>
